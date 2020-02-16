5b76063487f861d59cbd48b8d09e564b50e7a6ef
edwin siebes <esiebes@gmail.com>
2020-02-16 18:29:52 +0100

    string replaced with variable

--- tests/ChocolateyPublish.tests.ps1
+++ tests/ChocolateyPublish.tests.ps1
@@ -4,16 +4,19 @@ Describe "Chocolatey Publish functions" {
     $nuspecFileLocation = "$PSScriptRoot\assets\TestChocoPackage\testchocopackage.nuspec"
     $OutDirPath = (New-TemporaryDirectory).fullname
     $source = (New-TemporaryDirectory).fullname
+    $packagename = "testchocopackage.1.0.0.nupkg"
     Invoke-ChocoPack -NuSpecFilePath $nuspecFileLocation -OutputFolderPath $OutDirPath -UseBuildinChoco 
     it "Chocopackage should exist" { 
-        Test-Path "$OutDirPath\testchocopackage.1.0.0.nupkg" 
+        Test-Path "$OutDirPath\$packagename" 
     }
     it "Choco publish should not throw error to dir" {
-        { Publish-Chocopackage -PackagePath "$OutDirPath\testchocopackage.1.0.0.nupkg" -Source $source -Force} |
+        { Publish-Chocopackage -PackagePath "$OutDirPath\$packagename" -Source $source -Force} |
         Should Not throw
     }
 
     it "Published package should exist" {
-        Test-Path "$source\testchocopackage.1.0.0.nupkg"
+        Test-Path "$source\$packagename"
     }
+
+    #TODO add test with http(s) nuget/chocolatey stream/server
 }
